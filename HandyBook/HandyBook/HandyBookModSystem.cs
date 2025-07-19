77b8dff3e14a20c486e835afabb8b5daf66c366e
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2025-07-19 19:42:10 -0400

    Create project
    Errors galore

---
+++ HandyBook/HandyBook/HandyBookModSystem.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace HandyBook;
+
+public class HandyBookModSystem : ModSystem
+{
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+    public override void Start(ICoreAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod: " + api.Side);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("handybook:hello"));
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("handybook:hello"));
+    }
+}
